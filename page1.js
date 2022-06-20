58f88b953c154cec6b78a2c055b1aa949015810d
D7_63041_PrashantN <prashantnewase017@gmail.com>
2022-06-20 11:29:21 +0530

    Add functionality added

--- page1.js
+++ page1.js
@@ -8,3 +8,20 @@ function isEven(number) {
 
 isEven(10)
 isEven(11)
+
+function isOdd(number) {
+  if (number % 2 != 0) {
+    console.log(`${number} is ODD`)
+  } else {
+    console.log(`${number} is NOT ODD`)
+  }
+}
+
+isOdd(10)
+isOdd(11)
+
+function add(p1, p2) {
+  console.log(`${p1} + ${p2} = ${p1 + p2}`)
+}
+
+add(10, 20)
